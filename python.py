77b993617bbd88b17cd7d7666b872a94dc83b0e2
ramishraza <60193802+ramishraza@users.noreply.github.com>
2020-01-22 23:40:53 +0500

    python introduction

---
+++ python.py
@@ -0,0 +1,4 @@
+welcome to python in anaconda editor but you can use that you want 
+And also you can use many other  editor use for python programming its our first step that we  complete
+second we save a file in our PC and than
+now we start a programming
